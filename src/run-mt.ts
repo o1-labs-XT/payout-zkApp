189fe230cbafd72ca2c8cbb0017f051c54e16f49
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2025-12-21 16:37:54 -0300

    Add script to deploy and interact with payout zkapp on Mesa Testnet

---
+++ src/run-mt.ts
@@ -0,0 +1,171 @@
+import {
+  UInt64,
+  Mina,
+  PrivateKey,
+  PublicKey,
+  fetchAccount,
+  AccountUpdate,
+} from 'o1js';
+import { PayoutZkapp } from './Payout.js';
+
+import dotenv from 'dotenv';
+import { logTxInfo } from './helpers.js';
+
+dotenv.config();
+
+const MINA_NANO = 1e9;
+const MINA_NODE_ENDPOINT =
+  'https://plain-1-graphql.mina-mesa-network.gcp.o1test.net/graphql';
+const MINA_ARCHIVE_ENDPOINT = 'placeholder'; // 'https://plain-1-graphql.mesa-archive-node-api.gcp.o1test.net/graphql'
+
+const proofsEnabled = true;
+const logsEnabled = true;
+
+console.time('compile...');
+if (proofsEnabled) await PayoutZkapp.compile();
+console.timeEnd('compile...');
+
+// Set up the Mina Mesa Testnet
+const Mesa = Mina.Network({
+  mina: MINA_NODE_ENDPOINT,
+  archive: MINA_ARCHIVE_ENDPOINT,
+});
+Mina.setActiveInstance(Mesa);
+
+const payerPrivKeyBase58 = process.env.PAYOUT_SENDER_KEY;
+const requesterPrivKeyBase58 = process.env.PAYOUT_REQUEST_KEY;
+
+if (!payerPrivKeyBase58 || !requesterPrivKeyBase58) {
+  throw new Error('Missing private keys in .env');
+}
+
+const payerPrivateKey = PrivateKey.fromBase58(payerPrivKeyBase58);
+const requesterPrivateKey = PrivateKey.fromBase58(requesterPrivKeyBase58);
+
+// Derive public keys
+const payerPublicKey = payerPrivateKey.toPublicKey();
+const requesterPublicKey = requesterPrivateKey.toPublicKey();
+
+console.log('Payer PK:', payerPublicKey.toBase58());
+console.log('Requester PK:', requesterPublicKey.toBase58());
+
+// Set up the zkapp account
+let zkappPrivateKey = PrivateKey.random();
+let zkappAddress = zkappPrivateKey.toPublicKey();
+let zkapp = new PayoutZkapp(zkappAddress);
+
+console.log('Deploying zkApp...');
+await deployZkapp(zkapp, payerPrivateKey, zkappPrivateKey);
+
+const requestTx = await Mina.transaction(
+  { sender: requesterPublicKey },
+  async () => {
+    //! Test only; otherwise, for security reason we would have added a nullifer
+    //! scheme for accounts that already had a filled payout request before
+    await zkapp.requestPayout(UInt64.from(0.5 * MINA_NANO));
+    await zkapp.requestPayout(UInt64.from(0.5 * MINA_NANO));
+    await zkapp.requestPayout(UInt64.from(0.5 * MINA_NANO));
+    await zkapp.requestPayout(UInt64.from(0.5 * MINA_NANO));
+    await zkapp.requestPayout(UInt64.from(0.5 * MINA_NANO));
+
+    await zkapp.requestPayout(UInt64.from(0.5 * MINA_NANO));
+    await zkapp.requestPayout(UInt64.from(0.5 * MINA_NANO));
+  }
+);
+
+console.log('Request Transaction Info:', logTxInfo(requestTx), 'request');
+await waitTransactionAndFetchAccount(requestTx, [requesterPrivateKey]);
+
+console.log(
+  'Payout Requester balance before: ',
+  Mina.getAccount(requesterPublicKey).balance.toBigInt() / 1_000_000_000n
+);
+
+const payoutTx = await Mina.transaction(
+  { sender: payerPublicKey },
+  async () => {
+    await zkapp.payout(UInt64.from(2e9));
+  }
+);
+
+console.log('Payout Transaction Info:', logTxInfo(payoutTx));
+await waitTransactionAndFetchAccount(payoutTx, [payerPrivateKey]);
+
+console.log(
+  'Payout Requester balance after: ',
+  Mina.getAccount(requesterPublicKey).balance.toBigInt() / 1_000_000_000n
+);
+
+/// HELPER FUNCTIONS
+
+function log(...args: any[]) {
+  if (logsEnabled) {
+    console.log(...args);
+  }
+}
+
+/**
+ * Wait for a transaction to be included in a block and fetch the account.
+ * @param tx The transaction to wait for
+ * @param keys The keys to sign the transaction
+ * @param accountsToFetch The accounts to fetch after the transaction is included
+ */
+async function waitTransactionAndFetchAccount(
+  tx: Awaited<ReturnType<typeof Mina.transaction>>,
+  keys: PrivateKey[],
+  accountsToFetch?: PublicKey[]
+) {
+  try {
+    log('\nProving and sending transaction');
+    await tx.prove();
+    const pendingTransaction = await tx.sign(keys).send();
+
+    log('Waiting for transaction to be included in a block');
+
+    console.time('Mesa: Pending transaction');
+    const status = await pendingTransaction.safeWait();
+    console.timeEnd('Mesa: Pending transaction');
+
+    if (status.status === 'rejected') {
+      log('Transaction rejected', JSON.stringify(status.errors));
+      throw new Error(
+        'Transaction was rejected: ' + JSON.stringify(status.errors)
+      );
+    }
+
+    if (accountsToFetch) {
+      await fetchAccounts(accountsToFetch);
+    }
+  } catch (error) {
+    log('error', error);
+    throw error;
+  }
+}
+
+/**
+ * Fetch given accounts from the Mina to local cache.
+ * @param accounts List of account public keys to fetch
+ */
+async function fetchAccounts(accounts: PublicKey[]) {
+  for (let account of accounts) {
+    await fetchAccount({ publicKey: account }, MINA_NODE_ENDPOINT);
+  }
+}
+
+async function deployZkapp(
+  zkapp: PayoutZkapp,
+  deployerKey: PrivateKey,
+  zkappPrivateKey: PrivateKey
+) {
+  const deployerAccount = deployerKey.toPublicKey();
+  const tx = await Mina.transaction({ sender: deployerAccount }, async () => {
+    AccountUpdate.fundNewAccount(deployerAccount);
+    await zkapp.deploy();
+  });
+
+  await waitTransactionAndFetchAccount(
+    tx,
+    [deployerKey, zkappPrivateKey],
+    [zkappAddress]
+  );
+}
