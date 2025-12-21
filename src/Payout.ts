76334a94ded49405daa39874e220b6ffd71ee21d
Shigoto-dev19 <shigoto-dev19@protonmail.com>
2025-12-21 10:58:08 -0300

    Clean and add payout zkApp code

---
+++ src/Payout.ts
@@ -0,0 +1,106 @@
+import {
+  SmartContract,
+  Struct,
+  state,
+  State,
+  method,
+  Field,
+  Reducer,
+  PublicKey,
+  UInt64,
+  AccountUpdate,
+  Provable,
+  Bool,
+} from 'o1js';
+
+class ReceiptDetails extends Struct({
+  sender: PublicKey,
+  recipient: PublicKey,
+  amount: UInt64,
+  count: Field,
+  totalAmount: Field,
+}) {}
+
+class RequestDetails extends Struct({
+  recipient: PublicKey,
+  amount: UInt64,
+  isPending: Bool, // this is to ignore dummy actions // bool false by default
+}) {}
+
+class ReducedType extends Struct({
+  updatedCount: Field,
+  updatedTotal: Field,
+}) {}
+
+export class PayoutZkapp extends SmartContract {
+  @state(Field) counter = State<Field>();
+  @state(Field) total = State<Field>();
+  @state(Field) actionState = State<Field>();
+
+  reducer = Reducer({ actionType: RequestDetails });
+
+  events = {
+    receipt: ReceiptDetails,
+  };
+
+  init() {
+    super.init();
+    this.actionState.set(this.reducer.getActions().hash);
+  }
+
+  @method async requestPayout(amount: UInt64) {
+    const sender = this.sender.getAndRequireSignature();
+    this.reducer.dispatch(
+      new RequestDetails({ recipient: sender, amount, isPending: Bool(true) })
+    );
+  }
+
+  @method async payout(amount: UInt64) {
+    let actionState = this.actionState.getAndRequireEquals();
+    let pendingActions = this.reducer.getActions({
+      fromActionState: actionState,
+    });
+
+    let sender = this.sender.getUnconstrained();
+    const count = this.counter.getAndRequireEquals();
+    const total = this.total.getAndRequireEquals();
+
+    let { updatedCount, updatedTotal } = this.reducer.reduce(
+      pendingActions,
+      ReducedType,
+      (_state: ReducedType, action: RequestDetails) => {
+        const inRange = action.amount.lessThanOrEqual(amount);
+        const condition = action.isPending.and(inRange);
+
+        const payer = AccountUpdate.createIf(condition, sender);
+        payer.requireSignature();
+        payer.send({ to: action.recipient, amount: action.amount });
+
+        const updatedCount = Provable.if(condition, count.add(1), count);
+        const updatedTotal = Provable.if(
+          condition,
+          total.add(action.amount.value),
+          total
+        );
+
+        const eventData = new ReceiptDetails({
+          sender,
+          recipient: action.recipient,
+          amount: action.amount,
+          count: updatedCount,
+          totalAmount: updatedTotal,
+        });
+
+        // for (let i = 0; i < 21; i++) // uncomment if you want to emit events more than the 1024 limit
+        this.emitEventIf(action.isPending, 'receipt', eventData);
+
+        return new ReducedType({ updatedCount, updatedTotal });
+      },
+      new ReducedType({ updatedCount: count, updatedTotal: total })
+    );
+
+    this.actionState.set(pendingActions.hash);
+    this.counter.set(updatedCount);
+    this.total.set(updatedTotal);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { Add } from './Add.js';
+import { PayoutZkapp } from './Payout.js';
 
-export { Add };
+export { PayoutZkapp };
